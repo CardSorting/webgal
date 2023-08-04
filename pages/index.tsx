52657a14fd157be8074247f54161559e04e2cac7
CardSorting <115497755+CardSorting@users.noreply.github.com>
2023-08-04 02:11:38 +0000

    flex-wrap

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,15 +6,15 @@ import ImageForm from '../components/imageform';
 const HomePage: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const navStyle = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: '10px 20px',
-    backgroundColor: '#f5f5f5',
-    boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
-    flexWrap: 'wrap'
-  };
+  const navStyle: React.CSSProperties = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  padding: '10px 20px',
+  backgroundColor: '#f5f5f5',
+  boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
+  flexWrap: 'wrap'
+};
 
   const linkContainerStyle = {
     display: isOpen ? 'block' : 'none',
